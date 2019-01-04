b2e896c22a9ce4c296ffc998f0c354f27aacdd51
Michael McKay <mamckay89@gmail.com>
2019-01-03 17:51:43 -0700

    Initial commit

---
+++ routes/reviews.js
@@ -0,0 +1,120 @@
+'use strict';
+
+const express = require('express');
+const mongoose = require('mongoose');
+//const passport = require('passport');
+const Review = require('../models/review');
+const router = express.Router();
+
+//specify the authentication
+// gonna work on this later
+// router.use('/', passport.authenticate('jwt', { session: false, failWithError: true }));
+/* ========== GET/READ ALL ITEMS ========== */
+router.get('/', (req, res, next) => {
+  console.log(req.body);
+  Review.find({})
+    .sort('name')
+    .then(results => {
+      res.json(results);
+    })
+    .catch(err => {
+      next(err);
+    });
+});
+
+/* ========== GET/READ A SINGLE ITEM ========== */
+router.get('/:id', (req, res, next) => {
+    console.log(req.body);
+    const id = req.params.id;
+
+    Review.find({_id: id})
+    .sort('name')
+    .then(results => {
+      res.json(results);
+    })
+    .catch(err => {
+      next(err);
+    });
+});
+
+/* Delete Review */
+
+router.delete('/:id', (req,res,next) => {
+    const id = req.params.id;
+
+    Review.findOneAndRemove({_id: id})
+    .sort('name')
+    .then(results => {
+      res.json(results);
+    })
+    .catch(err => {
+      next(err);
+    });
+})
+
+/* Post Review */
+router.post('/', (req, res, next) => {
+  console.log(req.body);
+  const { name, genre, description } = req.body;
+  console.log(name);
+  const newReview = { name, genre, description, imgUrl };
+
+  // Checking for improper input from the user
+  // Check for bad ids
+  if (!name) {
+    const err = new Error('Missing `name` in request body');
+    err.status = 400;
+    return next(err);
+  }
+
+  Review.create(newReview)
+    .then(result => {
+      res.location(`${req.originalUrl}/${result.id}`).status(201).json(result);
+    })
+    .catch(err => {
+      if (err.code === 11000) {
+        err = new Error('Folder name already exists');
+        err.status = 400;
+      }
+      next(err);
+    });
+});
+
+router.put('/:id', (req, res, next) => {
+  const { id } = req.params;
+  const { name, description, genre, imgUrl } = req.body;
+  // const userId = req.user.id;
+  // Checking for improper input from the user
+  // Check for bad ids
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    const err = new Error('The `id` is not valid');
+    err.status = 400;
+    return next(err);
+  }
+
+  if (!name) {
+    const err = new Error('Missing `name` in request body');
+    err.status = 400;
+    return next(err);
+  }
+
+  const updateReview = { name, description , genre };
+
+  Review.findOneAndUpdate({_id: id} , updateReview, { new: true })
+    .then(result => {
+      if (result) {
+        res.json(result);
+      } else {
+        next();
+      }
+    })
+    .catch(err => {
+      if (err.code === 11000) {
+        err = new Error('Folder name already exists');
+        err.status = 400;
+      }
+      next(err);
+    });
+});
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,71 @@
+'use strict';
+
+/* 
+mongoimport --db doublejump --collection reviews --drop --file ./primer-review-dataset.json
+*/
+
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const { PORT, CLIENT_ORIGIN, DATABASE_URL } = require('./config');
+const passport = require('passport');
+const reviewRouter = require('./routes/reviews');
+const { router: usersRouter } = require('./users');
+const { router: authRouter, localStrategy, jwtStrategy } = require('./auth');
+mongoose.Promise = global.Promise;
+const app = express();
+
+
+app.use(
+  morgan(process.env.NODE_ENV === 'production' ? 'common' : 'dev', {
+    skip: (req, res) => process.env.NODE_ENV === 'test'
+  })
+);
+
+app.use(
+  cors({
+    origin: CLIENT_ORIGIN
+  })
+);
+app.use(express.json());
+
+// Find what this actually does
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
+  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
+  if (req.method === 'OPTIONS') {
+    return res.send(204);
+  }
+  next();
+});
+
+passport.use(localStrategy);
+passport.use(jwtStrategy);
+// begin adding routers
+app.use('/users/', usersRouter);
+app.use('/auth/', authRouter);
+const jwtAuth = passport.authenticate('jwt', { session: false });
+app.use('/reviews', reviewRouter);
+
+if (require.main === module) {
+  // Connect to DB and Listen for incoming connections
+  mongoose.connect(DATABASE_URL)
+    .then(instance => {
+      const conn = instance.connections[0];
+      console.info(`Connected to: mongodb://${conn.host}:${conn.port}/${conn.name}`);
+    })
+    .catch(err => {
+      console.error(err);
+    });
+
+  app.listen(PORT, function () {
+    console.info(`Server listening on ${this.address().port}`);
+  }).on('error', err => {
+    console.error(err);
+  });
+}
+
+module.exports = { app };
