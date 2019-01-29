b608ac009a4f1a81c2afff4d021af5e244a74c8f
Michael McKay <mamckay89@gmail.com>
2019-01-28 19:17:14 -0700

    updated ignore file

--- users/router.js
+++ users/router.js
@@ -1,38 +1,43 @@
-'use strict';
-const express = require('express');
-const bodyParser = require('body-parser');
-const passport = require('passport');
-const {User} = require('./models');
+"use strict";
+const express = require("express");
+const bodyParser = require("body-parser");
+const passport = require("passport");
+const { User } = require("./models");
 const router = express.Router();
 const jsonParser = bodyParser.json();
-const { ExtractJwt } = require('passport-jwt');
+const { ExtractJwt } = require("passport-jwt");
 
-
-const jwtAuth = passport.authenticate('jwt', { session: false });
+const jwtAuth = passport.authenticate("jwt", { session: false });
 // Post to register a new user
-router.post('/', jsonParser, (req, res) => {
-  const requiredFields = ['username', 'password','address'];
+router.post("/", jsonParser, (req, res) => {
+  const requiredFields = ["username", "password", "address"];
   const missingField = requiredFields.find(field => !(field in req.body));
 
   if (missingField) {
     return res.status(422).json({
       code: 422,
-      reason: 'ValidationError',
-      message: 'Missing field',
+      reason: "ValidationError",
+      message: "Missing field",
       location: missingField
     });
   }
 
-  const stringFields = ['username', 'password', 'firstName', 'lastName','address'];
+  const stringFields = [
+    "username",
+    "password",
+    "firstName",
+    "lastName",
+    "address"
+  ];
   const nonStringField = stringFields.find(
-    field => field in req.body && typeof req.body[field] !== 'string'
+    field => field in req.body && typeof req.body[field] !== "string"
   );
 
   if (nonStringField) {
     return res.status(422).json({
       code: 422,
-      reason: 'ValidationError',
-      message: 'Incorrect field type: expected string',
+      reason: "ValidationError",
+      message: "Incorrect field type: expected string",
       location: nonStringField
     });
   }
@@ -44,7 +49,7 @@ router.post('/', jsonParser, (req, res) => {
   // trimming them and expecting the user to understand.
   // We'll silently trim the other fields, because they aren't credentials used
   // to log in, so it's less of a problem.
-  const explicityTrimmedFields = ['username', 'password'];
+  const explicityTrimmedFields = ["username", "password"];
   const nonTrimmedField = explicityTrimmedFields.find(
     field => req.body[field].trim() !== req.body[field]
   );
@@ -52,8 +57,8 @@ router.post('/', jsonParser, (req, res) => {
   if (nonTrimmedField) {
     return res.status(422).json({
       code: 422,
-      reason: 'ValidationError',
-      message: 'Cannot start or end with whitespace',
+      reason: "ValidationError",
+      message: "Cannot start or end with whitespace",
       location: nonTrimmedField
     });
   }
@@ -71,44 +76,42 @@ router.post('/', jsonParser, (req, res) => {
   };
   const tooSmallField = Object.keys(sizedFields).find(
     field =>
-      'min' in sizedFields[field] &&
-            req.body[field].trim().length < sizedFields[field].min
+      "min" in sizedFields[field] &&
+      req.body[field].trim().length < sizedFields[field].min
   );
   const tooLargeField = Object.keys(sizedFields).find(
     field =>
-      'max' in sizedFields[field] &&
-            req.body[field].trim().length > sizedFields[field].max
+      "max" in sizedFields[field] &&
+      req.body[field].trim().length > sizedFields[field].max
   );
 
   if (tooSmallField || tooLargeField) {
     return res.status(422).json({
       code: 422,
-      reason: 'ValidationError',
+      reason: "ValidationError",
       message: tooSmallField
-        ? `Must be at least ${sizedFields[tooSmallField]
-          .min} characters long`
-        : `Must be at most ${sizedFields[tooLargeField]
-          .max} characters long`,
+        ? `Must be at least ${sizedFields[tooSmallField].min} characters long`
+        : `Must be at most ${sizedFields[tooLargeField].max} characters long`,
       location: tooSmallField || tooLargeField
     });
   }
   const cart = [];
-  let {username, password, firstName, lastName, address} = req.body;
+  let { username, password, firstName, lastName, address } = req.body;
   // Username and password come in pre-trimmed, otherwise we throw an error
   // before this
   firstName = firstName.trim();
   lastName = lastName.trim();
 
-  return User.find({username})
+  return User.find({ username })
     .count()
     .then(count => {
       if (count > 0) {
         // There is an existing user with the same username
         return Promise.reject({
           code: 422,
-          reason: 'ValidationError',
-          message: 'Username already taken',
-          location: 'username'
+          reason: "ValidationError",
+          message: "Username already taken",
+          location: "username"
         });
       }
       // If there is no existing user, hash the password
@@ -130,10 +133,10 @@ router.post('/', jsonParser, (req, res) => {
     .catch(err => {
       // Forward validation errors on to the client, otherwise give a 500
       // error because something unexpected has happened
-      if (err.reason === 'ValidationError') {
+      if (err.reason === "ValidationError") {
         return res.status(err.code).json(err);
       }
-      res.status(500).json({code: 500, message: 'Internal server error'});
+      res.status(500).json({ code: 500, message: "Internal server error" });
     });
 });
 
@@ -142,70 +145,66 @@ router.post('/', jsonParser, (req, res) => {
 // if we're creating users. keep in mind, you can also
 // verify this in the Mongo shell.
 
-router.put('/cart',jwtAuth, (req, res, next) => {
+router.put("/cart", jwtAuth, (req, res, next) => {
   console.log(req.user);
-  const {cart} = req.body;
-  return  User.findOneAndUpdate({_id: req.user.id} , {cart}, { new: true })
-      .then(result => {
-        if (result) {
-          res.json(result);
-        } else {
-          next();
-        }
-      })
-      .catch(err => {
-        if (err.code === 11000) {
-          err = new Error('Could not update cart');
-          err.status = 400;
-        }
-        next(err);
-      });
+  const { cart } = req.body;
+  return User.findOneAndUpdate({ _id: req.user.id }, { cart }, { new: true })
+    .then(result => {
+      if (result) {
+        res.json(result);
+      } else {
+        next();
+      }
+    })
+    .catch(err => {
+      if (err.code === 11000) {
+        err = new Error("Could not update cart");
+        err.status = 400;
+      }
+      next(err);
+    });
 });
 
-router.put('/address',jwtAuth, (req, res, next) => {
+router.put("/address", jwtAuth, (req, res, next) => {
   console.log(req.user);
-  const {address} = req.body;
-  return  User.findOneAndUpdate({_id: req.user.id} , {address}, { new: true })
-      .then(result => {
-        if (result) {
-          res.json(result);
-        } else {
-          next();
-        }
-      })
-      .catch(err => {
-        if (err.code === 11000) {
-          err = new Error('Could not update cart');
-          err.status = 400;
-        }
-        next(err);
-      });
+  const { address } = req.body;
+  return User.findOneAndUpdate({ _id: req.user.id }, { address }, { new: true })
+    .then(result => {
+      if (result) {
+        res.json(result);
+      } else {
+        next();
+      }
+    })
+    .catch(err => {
+      if (err.code === 11000) {
+        err = new Error("Could not update cart");
+        err.status = 400;
+      }
+      next(err);
+    });
 });
 
-router.get('/cart',jwtAuth, (req, res, next) => {
-  
-  return User.findOne({_id: req.user.id})
+router.get("/cart", jwtAuth, (req, res, next) => {
+  return User.findOne({ _id: req.user.id })
     .then(user => res.json(user.serialize()))
-    .catch(err => res.status(500).json({message: 'Internal server error'}));
-})
+    .catch(err => res.status(500).json({ message: "Internal server error" }));
+});
 
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
   return User.find()
     .then(users => res.json(users.map(user => user.serialize())))
-    .catch(err => res.status(500).json({message: 'Internal server error'}));
+    .catch(err => res.status(500).json({ message: "Internal server error" }));
 });
 
-router.delete('/',jwtAuth, (req, res) => {
-  const id = req.user.id
-  return User.findOneAndDelete({_id:id})
+router.delete("/", jwtAuth, (req, res) => {
+  const id = req.user.id;
+  return User.findOneAndDelete({ _id: id })
     .then(users => res.json(users.serialize()))
-    .catch(err => res.status(500).json({message: 'Internal server error'}));
+    .catch(err => res.status(500).json({ message: "Internal server error" }));
 });
 
-module.exports = {router};
-
-
-
+module.exports = { router };
 
 // router.put('/:id', (req, res, next) => {
 //   const { id } = req.params;
